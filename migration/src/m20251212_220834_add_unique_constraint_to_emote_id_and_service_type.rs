0c8c31817c2bcedb9c3c22b329f27e4557011787
LinkTheDot <CirnoStrongest9909@hotmail.com>
2025-12-14 00:38:32 -0500

    Adds unique external_id + external_service emotes
    This commit removes the unique constraint on just external_id on emotes
    and makes the constraint the external_id + external_service. This is to
    prevent emotes with matching IDs from different services from
    conflicting.

--- migration/src/m20251212_220834_add_unique_constraint_to_emote_id_and_service_type.rs
+++ migration/src/m20251212_220834_add_unique_constraint_to_emote_id_and_service_type.rs
@@ -1,4 +1,7 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::prelude::*;
+
+const UNIQUE_EXTERNAL_SERVICE_EXTERNAL_ID_INDEX: &str = "idx_emote_external_service_external_id";
+const OLD_EXTERNAL_ID_UNIQUE_KEY: &str = "external_id";
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,19 +9,60 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
   async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-    todo!();
+    manager
+      .drop_index(
+        Index::drop()
+          .name(OLD_EXTERNAL_ID_UNIQUE_KEY)
+          .table(Emote::Table)
+          .to_owned(),
+      )
+      .await?;
+
+    manager
+      .create_index(
+        Index::create()
+          .name(UNIQUE_EXTERNAL_SERVICE_EXTERNAL_ID_INDEX)
+          .table(Emote::Table)
+          .col(Emote::ExternalId)
+          .col(Emote::ExternalService)
+          .unique()
+          .to_owned(),
+      )
+      .await?;
+
+    Ok(())
   }
 
   async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-    todo!();
+    manager
+      .drop_index(
+        Index::drop()
+          .name(UNIQUE_EXTERNAL_SERVICE_EXTERNAL_ID_INDEX)
+          .table(Emote::Table)
+          .to_owned(),
+      )
+      .await?;
+
+    manager
+      .create_index(
+        Index::create()
+          .name(OLD_EXTERNAL_ID_UNIQUE_KEY)
+          .table(Emote::Table)
+          .col(Emote::ExternalId)
+          .unique()
+          .to_owned(),
+      )
+      .await?;
+
+    Ok(())
   }
 }
 
 #[derive(Iden)]
 enum Emote {
   Table,
-  Id,
+  _Id,
   _Name,
-  _ExternalId,
-  _ExternalService,
+  ExternalId,
+  ExternalService,
 }
