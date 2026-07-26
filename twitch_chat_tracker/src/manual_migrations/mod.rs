a412e28785a7bbe06fc360aaf8bf728bac753022
LinkTheDot <94793263+LinkTheDot@users.noreply.github.com>
2026-07-26 13:34:52 -0400

    Add donation charts for spreadsheets
    This commit adds a script for a donation graph over time for
    spreadsheets.

--- twitch_chat_tracker/src/manual_migrations/mod.rs
+++ twitch_chat_tracker/src/manual_migrations/mod.rs
@@ -1,5 +1,6 @@
 pub mod calculate_time_graph_for_subathon;
 pub mod convert_message_third_party_emotes_into_emote_tables;
+pub mod donation_charts;
 pub mod fix_donations;
 pub mod mark_scrubbed_users;
 pub mod other;
